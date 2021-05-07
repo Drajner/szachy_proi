7d1b26dd094c0fa83d6fb95023f9ef369e802db7
Duckonaut <hasohasacz@gmail.com>
2021-05-07 13:03:31 +0200

    Added piece and position headers

---
+++ piece.h
@@ -0,0 +1,74 @@
+#pragma once
+
+#include <vector>
+#include <string>
+#include "position.h"
+
+#define Chessboard int // Placeholder so there aren't any arrors
+
+// Abstract representation of a chess piece
+class Piece {
+private:
+	// Position on the chessboard
+	Position position;
+	// Color of the piece. 1 is white, 0 is black
+	bool color;
+
+	// Full piece name, to be set in the constructor of an inheriting piece
+	std::string fullName;
+	// Short, 1 char representation (for display purposes in the << operator), to be set in the constructor of an inheriting piece
+	char chessboardRepresentation;
+public:
+	/**
+	 * Creates a new chess piece
+	 *
+	 * @param startPosition Starting position
+	 * @param isWhite Is the piece part of the white team? 
+	 */
+	Piece(Position startPosition, bool isWhite);
+
+	// Checks if this piece is part of the white team
+	bool is_white() const;
+
+	// Checks if this piece is part of the black team
+	bool is_black() const;
+
+	// Gets the full name of this piece
+	std::string get_full_name() const;
+
+	// Gets the short representation
+	char get_char_representation() const;
+
+	/**
+	 * Gets the possible moves of this piece on a chessboard
+	 *
+	 * @param chessboard Chessboard the piece will move on.
+	 * @return Vector of possible new positions to take
+	 */
+	virtual std::vector<Position> possible_moves(const Chessboard& chessboard) const = 0;
+
+	/**
+	 * Moves this piece to a new position on the chessboard. If move will cause an enemy piece to be captured, it will be removed from the chessboard
+	 *
+	 * @param chessboard Chessboard the piece will move on. 
+	 * @param newPosition New position for this piece
+	 */
+	void move_to(const Chessboard& chessboard, Position newPosition);
+
+	/**
+	 * Compares two chess pieces by name, color and position
+	 *
+	 * @param other Piece to compare to
+	 */
+	bool operator==(const Piece& other) const;
+
+	/**
+	 * Compares two chess pieces by name, color and position
+	 *
+	 * @param other Piece to compare to
+	 */
+	bool operator!=(const Piece& other) const;
+
+	// Appends the short char representation to the stream
+	friend std::ostream& operator<<(std::ostream& out, const Piece& piece);
+};
---
+++ position.h
@@ -0,0 +1,30 @@
+#pragma once
+#include <iostream>
+
+// Stores a position on the chessboard. Will limit positions to the range of 1-8, a-h
+struct Position {
+private:
+	int x_;
+	int y_;
+public:
+	Position(int posX, int posY);
+
+	// Sets the horizontal position. Will clamp it in the 1-8 range
+	void set_x(int newX);
+	// Sets the vertical position. Will clamp it in the 1-8 range
+	void set_y(int newY);
+	// Gets the horizontal position in int form
+	int get_x() const;
+	// Gets the vertical position in int form
+	int get_y() const;
+
+	// Compares two positions by x and y
+	bool operator==(const Position& other) const;
+	// Compares two positions by x and y
+	bool operator!=(const Position& other) const;
+
+	// Representation in form of "yx" where y will be converted to char from range a-h, x stays as a number
+	friend std::ostream& operator<<(std::ostream& out, const Position& pos);
+	// Accepts two forms of representation, "xy" and "yx". Char part will always be the y
+	friend std::istream& operator>>(std::istream& in, Position& pos);
+};
