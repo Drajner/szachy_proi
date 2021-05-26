b6942f0ef85ccc07b26554ee21ef5398e85ea948
Drajner <drajner@wp.pl>
2021-05-26 21:57:01 +0200

    added Player.cpp, modified Player.h

---
+++ Player.cpp
@@ -0,0 +1,47 @@
+#include <string>
+#include <vector>
+#include <memory>
+#include "position.h"
+#include "piece.h"
+#include "Chessboard.h"
+#include "Player.h"
+
+Player::Player(Color color, std::string playerName)
+{
+
+}
+
+std::vector<std::pair<std::shared_ptr<Piece>, Position>> Player::allPossibleMoves(const Chessboard& chessboard)
+{
+	std::vector<std::pair<std::shared_ptr<Piece>, Position>> moves;
+	for (auto piece : pieces_)
+	{
+		for (auto position : piece->possible_moves(chessboard))
+		{
+			moves.push_back(std::make_pair(piece, position));
+		}
+	}
+	return moves;
+}
+
+void Player::setColor(Color color)
+{
+	color_ = color;
+}
+
+std::string Player::getName()
+{
+	return name_;
+}
+
+Color Player::getColor()
+{
+	return color_;
+}
+
+bool Player::operator==(Player& player)
+{
+	if (this->getColor() == player.getColor() && this->getName() == player.getName()) return true;
+	else return false;
+}
+
--- Player.h
+++ Player.h
@@ -4,28 +4,38 @@
 #include <memory>
 #include "piece.h"
 #include "position.h"
+
+class Piece;
+
 class Player
 {
 private:
-	bool color; // 0 for black, 1 for white
-	std::string name;
+	Color color_; // 0 for black, 1 for white
+	std::string name_;
 
 	// TODO: Change to game/chessboard reference?
-	std::vector<std::unique_ptr<Piece>> pieces;
+	std::vector<std::shared_ptr<Piece>> pieces_;
 public:
 	// Player class constructor
-	Player(bool isWhite, bool isComputer, std::string playerName);
+	Player(Color color, std::string playerName);
 
 	// issues a move(will change depending on derivative class)
-	virtual void makeMove() =0;
+	virtual void makeMove() = 0;
 
 	// sets color
-	void setColor(bool isWhite); 
+	void setColor(Color color);
+
+	// gets name
+	std::string getName();
+
+	// gets name
+	Color getColor();
 
 	// gets all possible movements from individual pieces
-	std::vector<Position> allPossibleMoves();
+	std::vector<std::pair<std::shared_ptr<Piece>, Position>> allPossibleMoves(const Chessboard& chessboard);
 
-	bool operator==(const Player& player);
-	
+	// checks if players are the same
+	bool operator==(Player& player);
+
 };
 
