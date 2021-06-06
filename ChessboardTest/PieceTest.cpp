18b23e4611e647f87194444123b1f74ded1dbe8c
Duckonaut <hasohasacz@gmail.com>
2021-06-06 19:07:49 +0200

    More piece tests

--- ChessboardTest/PieceTest.cpp
+++ ChessboardTest/PieceTest.cpp
@@ -11,6 +11,38 @@ namespace ChessTest
 	TEST_CLASS(PieceTest)
 	{
 	public:
+		TEST_METHOD(TestNamesReperesentations)
+		{
+			Pawn whitePawn(Position(1, 1), White), blackPawn(Position(1, 1), Black);
+			Knight whiteKnight(Position(1, 1), White), blackKnight(Position(1, 1), Black);
+			Rook whiteRook(Position(1, 1), White), blackRook(Position(1, 1), Black);
+			Bishop whiteBishop(Position(1, 1), White), blackBishop(Position(1, 1), Black);
+			King whiteKing(Position(1, 1), White), blackKing(Position(1, 1), Black);
+			Queen whiteQueen(Position(1, 1), White), blackQueen(Position(1, 1), Black);
+
+			std::stringstream ss;
+
+			ss << whitePawn << blackPawn <<
+				whiteKnight << blackKnight << 
+				whiteRook << blackRook <<
+				whiteBishop << blackBishop << 
+				whiteKing << blackKing << 
+				whiteQueen << blackQueen;
+
+			Assert::IsTrue(ss.str() == "pPnNrRbBkKqQ");
+
+			ss = std::stringstream();
+
+			ss << whitePawn.full_name() << ' ' << blackPawn.full_name() << ' ' <<
+				whiteKnight.full_name() << ' ' << blackKnight.full_name() << ' ' <<
+				whiteRook.full_name() << ' ' << blackRook.full_name() << ' ' <<
+				whiteBishop.full_name() << ' ' << blackBishop.full_name() << ' ' <<
+				whiteKing.full_name() << ' ' << blackKing.full_name() << ' ' <<
+				whiteQueen.full_name() << ' ' << blackQueen.full_name();
+
+			Assert::IsTrue(ss.str() == "Pawn Pawn Knight Knight Rook Rook Bishop Bishop King King Queen Queen");
+		}
+
 		TEST_METHOD(TestPawnPossibleMovesBasic)
 		{
 			auto pawn = std::make_shared<Pawn>(Pawn(Position(1, 1), White));
